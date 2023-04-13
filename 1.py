7d12bd0a768dd09e09cd0f1c10e1a0817408e8d3
EC2 Default User <ec2-user@ip-172-31-7-123.ec2.internal>
2023-04-13 00:01:52 +0000

    Dicionario

---
+++ 1.py
@@ -0,0 +1,9 @@
+dicionario = {"gato": "chat", "cachorro": "chien", "cavalo": "cheval"}
+palavras = ['gato', 'lion', 'cavalo']
+
+
+for palavra in palavras:
+    if palavra in dicionario:
+        print(palavra, " -> ", dicionario[palavra])
+    else:
+        print(palavra, "não está no discionario")
---
+++ 10.py
@@ -0,0 +1,20 @@
+cadastro = {}
+
+while True:
+    nome = input("Digite o nome completo: ")
+    if nome == '':
+        break
+    
+    
+    idade = int(input("Digite a idade: "))
+    cidade = input("Digite a cidade: ")
+    
+    
+    cadastro[nome] = {"idade": idade, "cidade": cidade}
+    
+print("Cadastro: ")
+print(cadastro)
+for nome, dados in cadastro.items():
+    print(" - nome: ", nome)
+    print(" Idade: ", dados["idade"])
+    print(" Cidade: ", dados["cidade"])
